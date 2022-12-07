fd3fff45b989805aeb766fbb8f25fcb6265432a0
surenravi1407 <surenravi1407@gmail.com>
2022-12-07 17:15:46 +0200

    Store

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const path = require("path");
+const port = 8000;
+// Use the whole root as static files to be able to serve the html file and
+// the build folder
+app.use(express.static(path.join(__dirname, "/")));
+// Send html on '/'path
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, +"/index.html"));
+});
+// Create the server and listen on port
+app.listen(process.env.PORT || 8000, function() {
+    console.log(
+        "Express serve listening on port %d in %s mode",
+        this.address().port
+    );
+});
